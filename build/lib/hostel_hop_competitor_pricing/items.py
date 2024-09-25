e37ede5be01d6f296d0658be236758584eb98aeb
sam-parks <parkssa123@gmail.com>
2024-09-25 09:21:42 -0600

    updates

---
+++ build/lib/hostel_hop_competitor_pricing/items.py
@@ -0,0 +1,9 @@
+# items.py
+
+import scrapy
+
+class HostelItem(scrapy.Item):
+    property_hostelz_url = scrapy.Field()
+    property_name = scrapy.Field()
+    hostel_hop_property_id = scrapy.Field()
+    rooms = scrapy.Field()
